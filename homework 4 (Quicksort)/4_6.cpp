8bef761f1574b98f86c6bbf3fb98a712777b4598
Chatos_Xeno <105534510+Gamesunch@users.noreply.github.com>
2025-01-05 10:56:41 +0700

    this is a working 4_6

--- homework 4 (Quicksort)/4_6.cpp
+++ homework 4 (Quicksort)/4_6.cpp
@@ -1,108 +1,80 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
-//I know this code is shit but idk how to do it anymore. don't care anymore if better approch tell me, I would be please.
+string vectorToString(vector<int>& vec) {
+    string result;
+    for (int num : vec) {
+        result += to_string(num);
+    }
+    return result;
+}
+
+vector<char> findMostFrequentDigits(string& str) {
+    vector<char> frequentDigits;
+    int maxCount = 1, currentCount = 1;
+
+    for (size_t i = 1; i < str.size(); i++) {
+        if (str[i] == str[i - 1]) {
+            currentCount++;
+        } else {
+            currentCount = 1;
+        }
+
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            frequentDigits.clear();
+            frequentDigits.push_back(str[i]);
+        } else if (currentCount == maxCount) {
+            if (find(frequentDigits.begin(), frequentDigits.end(), str[i]) == frequentDigits.end()) {
+                frequentDigits.push_back(str[i]);
+            }
+        }
+    }
+
+    sort(frequentDigits.begin(), frequentDigits.end());
+    return frequentDigits;
+}
+
+string reduceConsecutiveDuplicates(string& str) {
+    string reduced;
+    reduced += str[0];
+    for (size_t i = 1; i < str.size(); i++) {
+        if (str[i] != str[i - 1]) {
+            reduced += str[i];
+        }
+    }
+    return reduced;
+}
+
 int main() {
-    // Example input
     int n;
     cin >> n;
 
-    int arr[n];
+    vector<int> numbers(n);
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> numbers[i];
     }
 
-    sort(arr, arr + n);
+    sort(numbers.begin(), numbers.end());
 
-    // Calculate the total number of digits
-    int size = 0;
-    for (int i = 0; i < n; i++) {
-        int num = arr[i];
-        int digitCount = 0;
-        while (num > 0) {
-            digitCount++;
-            num /= 10;
-        }
-        size += digitCount;
-    }
+    // Output 1
+    string concatenated = vectorToString(numbers);
+    cout << concatenated << endl;
 
-    // Dynamically allocate memory for arr2
-    int* arr2 = new int[size]; 
-
-    // Convert from many digits into single digits
-    int index = 0; 
-    for (int i = 0; i < n; i++) {
-        int num = arr[i];
-        int temp[10];
-        int digitCount = 0;
-        while (num > 0) {
-            temp[digitCount++] = num % 10;
-            num /= 10;
-        }
-
-        for (int j = digitCount - 1; j >= 0; j--) {
-            arr2[index++] = temp[j];
-        }
-    }
-
-    // Print the resulting array (output 1)
-    for (int i = 0; i < size; i++) {
-        cout << arr2[i];
-    }
-    cout<<endl;
-
-    // Print mode (output 2)
-    // need to fix this code doesn't work like i expect, but now i need to rest.
-    int maxCount = -10000;
-    int tempCount = 0;
-    for(int i = 0;i<size;i++){
-      tempCount = 0;
-      for(int j = i;j<size;j++){
-        if(arr2[i] == arr2[j]){
-          tempCount++;
-        }
-        else{
-          break;
-        }
-
-        if(tempCount >= maxCount){
-          maxCount = tempCount;
-        }
-      }
-    }
-
-    for(int i = 0;i<size;i++){
-      tempCount = 0;
-      for(int j = i;j<size;j++){
-        if(arr2[i] == arr2[j]){
-          tempCount++;
-        }
-        else{
-          break;
-        }
-      }
-      if(tempCount == maxCount){
-          cout<<arr2[i]<<" ";
-        }
-    }
-    cout<<endl;
-
-    //output 3 (reduce same num)
-    for(int i = 0; i <= size; i++){
-      for(int j = i + 1; j <= size; j++){
-
-        if(arr2[i] != arr2[j]){
-          cout<<arr2[i];
-          break;
-        }
-        else{
-          break;
-        }
-      }
+    // Output 2
+    vector<char> mostFrequent = findMostFrequentDigits(concatenated);
+    for (char digit : mostFrequent) {
+        cout << digit << " ";
     }
     cout << endl;
 
+    // Output 3
+    cout << reduceConsecutiveDuplicates(concatenated) << endl;
+
     return 0;
 }
+
